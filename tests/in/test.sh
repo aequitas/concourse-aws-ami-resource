6e7032b05cb35653c700446b1e961b33b01c45e6
Johan Bloemberg <johan.bloemberg@avira.com>
2016-03-09 17:54:13 +0100

    Create id file for 'in'

--- tests/in/test.sh
+++ tests/in/test.sh
@@ -8,7 +8,13 @@ export DEBUG=1
 
 input='{"version":{"ImageId": "ami-0123456"}}'
 
+mkdir -p /tmp/build/get
+
 output=$(echo $input | /opt/resource/in /tmp/build/get)
+
+test -f /tmp/build/get/id
+
+grep "ami-0123456" /tmp/build/get/id 1>&2
 
 echo $output | jq -er '.version.ImageId == "ami-0123456"'
 
