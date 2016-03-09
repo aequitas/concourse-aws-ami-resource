23ddb9321b9c1becc02bfbece59741b6cd324597
Johan Bloemberg <johan.bloemberg@avira.com>
2016-03-09 13:27:42 +0100

    Add option to terminate instance after ami creation

--- tests/out/test.sh
+++ tests/out/test.sh
@@ -6,11 +6,28 @@ set -xe
 export PATH="$(realpath $(dirname $0)):$PATH"
 export DEBUG=1
 
+# test output with terminating instance
+
+input='{"params":{"name": "test", "terminate": true}}'
+
+mkdir -p /tmp/build/put/ami-id/
+echo "i-12345678" > /tmp/build/put/ami-id/id
+
+touch /tmp/i-12345678
+output=$(echo $input | /opt/resource/out /tmp/build/put)
+test ! -f /tmp/i-12345678
+
+echo $output | jq -er '.version.ImageId == "ami-0123456"'
+
+# test output without terminating instance
+
 input='{"params":{"name": "test"}}'
 
 mkdir -p /tmp/build/put/ami-id/
 echo "i-12345678" > /tmp/build/put/ami-id/id
 
+touch /tmp/i-12345678
 output=$(echo $input | /opt/resource/out /tmp/build/put)
+test -f /tmp/i-12345678
 
 echo $output | jq -er '.version.ImageId == "ami-0123456"'
--- tests/tests.sh
+++ tests/tests.sh
@@ -3,9 +3,10 @@
 cd $(dirname $0)
 for test in $(ls */test.sh);do
     log=$(mktemp /tmp/test-log.XXXXXX)
-    exec 3>&2 # make stderr available as fd 3 for the result
-    exec 2>$log
 
     echo running $test
-    $test || cat $log 1>&3
+    if ! $test 2>$log;then
+        cat $log 1>&2
+        exit 1
+    fi
 done
