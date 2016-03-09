10b502443a89722699554e31b79c05a6cb9d4950
Johan Bloemberg <johan.bloemberg@avira.com>
2016-03-09 15:59:49 +0100

    Fix metadata for in

--- tests/in/test.sh
+++ tests/in/test.sh
@@ -11,3 +11,6 @@ input='{"version":{"ImageId": "ami-0123456"}}'
 output=$(echo $input | /opt/resource/in /tmp/build/get)
 
 echo $output | jq -er '.version.ImageId == "ami-0123456"'
+
+# test metadata output
+echo $output | jq -er '.metadata|map({"key":.name, "value":.value})|from_entries|.Name == "test"'
